c81ce33601e53449f82488b554baa05e36b360dc
VASU-XD <79446681+VASU-XD@users.noreply.github.com>
2021-07-09 14:44:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-"""
-RadioPlayerV2, Telegram Voice Chat Userbot
-Copyright (C) 2021  Asm Safone
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>
-"""
-
 from pyrogram import Client, idle, filters
 import os
 from threading import Thread
@@ -44,7 +29,7 @@ bot.run(main())
 bot.start()
 @bot.on_message(filters.command("restart") & filters.user(Config.ADMINS))
 def restart(client, message):
-    message.reply_text("**Restarting... Join @AsmSafone!**")
+    message.reply_text("**Restarting...!! Made with ❤️ by @xxvasu!**")
     Thread(
         target=stop_and_restart
         ).start()
